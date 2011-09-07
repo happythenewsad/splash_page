f6c74ecd869004a3533174e615f01f7941e9d137
Peter Kong <peter.kong@lmnsolutions.com>
2011-09-07 00:30:08 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PwSplash::Application.config.secret_token = 'fd9ab6d6362c881d82cc533a9a76e643fad3361cafd224e3cf7ced70e9f1ca09998358bd5b59301d69e48d0a622d07ccc0683b4233d62086a70c4a39a70acc08'
